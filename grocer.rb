55f4f164f0c82f8566f43447c87fbac451e93746
Carl Parm <carlparm@gmail.com>
2020-01-23 23:46:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,8 +3,6 @@ def find_item_by_name_in_collection(name, collection)
   while i < collection.length 
     if name == collection[i][:item]
       return collection[i]
-    else
-      return nil
   end
   i += 1
 end
