da2e63968f7fdcd9954fa480219554e8ab337f7e
Carl Parm <carlparm@gmail.com>
2020-01-24 00:00:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
   while i < cart.length
     new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart)
     if new_cart_item
-      
+      new_cart_item
   i += 1  
   end
 
