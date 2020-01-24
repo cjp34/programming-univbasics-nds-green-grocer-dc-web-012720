f7e50e09d1a4504f847e7ce96a10110eeb00432b
Carl Parm <carlparm@gmail.com>
2020-01-24 01:02:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,5 +70,12 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
-  clearanced_cart = apply_clearance(couponed_cart)
+  final_cart = apply_clearance(couponed_cart)
+  
+  total = 0
+  i = 0
+  while i < final_cart.length
+    total += final_cart[i][:price] * final_cart[i][:count]
+  i += 1
+  end
 end
